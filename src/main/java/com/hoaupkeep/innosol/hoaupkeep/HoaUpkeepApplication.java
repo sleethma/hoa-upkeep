f456ccb5983be69261c56414351d93a34200f96e
Michael Sleeth <sleeth.ma@gmail.com>
2018-12-02 12:59:39 -0600

    Initial Commit

---
+++ src/main/java/com/hoaupkeep/innosol/hoaupkeep/HoaUpkeepApplication.java
@@ -0,0 +1,12 @@
+package com.hoaupkeep.innosol.hoaupkeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoaUpkeepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoaUpkeepApplication.class, args);
+	}
+}
