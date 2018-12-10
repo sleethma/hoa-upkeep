484d653709514f294e1bacc61b063c1058fbc6b8
Michael Sleeth <sleeth.ma@gmail.com>
2018-12-10 14:33:33 -0600

    Create Controller and index page for contractor. Closes #7

---
+++ hoaup-keep-web/src/main/java/com/hoaupkeep/innosol/web/controllers/ContractorController.java
@@ -0,0 +1,13 @@
+package com.hoaupkeep.innosol.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ContractorController {
+
+    @GetMapping({"contractors/index", "contractors/index.html", "contractors"})
+    public String getListView(){
+        return "contractors/index";
+    }
+}
