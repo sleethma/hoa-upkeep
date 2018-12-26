3a446033394b55cab5dbac6a2bb17ca9a86bd052
Michael Sleeth <sleeth.ma@gmail.com>
2018-12-26 12:41:20 -0600

    Adds Specialties to Contractor. Closes #27

--- hoa-upkeep-data/src/main/java/com/hoaupkeep/innosol/models/Contractor.java
+++ hoa-upkeep-data/src/main/java/com/hoaupkeep/innosol/models/Contractor.java
@@ -1,38 +1,16 @@
 package com.hoaupkeep.innosol.models;
 
 import com.hoaupkeep.innosol.services.ContractorService;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Set;
 
+@Slf4j
+@Getter
+@Setter
 public class Contractor extends Person {
 
-//    @Override
-//    public Home findByLastName(String lastName) {
-//        return null;
-//    }
-//
-//    @Override
-//    public Set<Contractor> findAll() {
-//        return null;
-//    }
-//
-//    @Override
-//    public Contractor findById(Long aLong) {
-//        return null;
-//    }
-//
-//    @Override
-//    public Contractor save(Contractor object) {
-//        return null;
-//    }
-//
-//    @Override
-//    public void delete(Contractor object) {
-//
-//    }
-//
-//    @Override
-//    public void deleteById(Long aLong) {
-//
-//    }
+Set<ContractorSpecialty> specialties;
 }
---
+++ hoa-upkeep-data/src/main/java/com/hoaupkeep/innosol/models/ContractorSpecialty.java
@@ -0,0 +1,13 @@
+package com.hoaupkeep.innosol.models;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Getter
+@Setter
+public class ContractorSpecialty extends BaseEntity {
+
+    private String specialty;
+}
