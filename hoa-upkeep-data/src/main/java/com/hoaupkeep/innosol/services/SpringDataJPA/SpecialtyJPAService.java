415aaeddc8ce280c49555bddde3fb050775175c8
Michael Sleeth <sleeth.ma@gmail.com>
2019-01-02 14:05:33 -0600

    Create Spring Data JPA Specialty Service. Closes #44.

---
+++ hoa-upkeep-data/src/main/java/com/hoaupkeep/innosol/services/SpringDataJPA/SpecialtyJPAService.java
@@ -0,0 +1,51 @@
+package com.hoaupkeep.innosol.services.SpringDataJPA;
+
+import com.hoaupkeep.innosol.models.ContractorSpecialty;
+import com.hoaupkeep.innosol.models.Home;
+import com.hoaupkeep.innosol.repos.ContractorSpecialtyRepo;
+import com.hoaupkeep.innosol.repos.HomeRepo;
+import com.hoaupkeep.innosol.services.HomesService;
+import com.hoaupkeep.innosol.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("SpringDataJPA")
+public class SpecialtyJPAService implements SpecialtyService {
+
+    ContractorSpecialtyRepo specialtyRepo;
+
+    public SpecialtyJPAService(ContractorSpecialtyRepo specialtyRepo) {
+        this.specialtyRepo = specialtyRepo;
+    }
+
+    @Override
+    public Set<ContractorSpecialty> findAll() {
+        Set<ContractorSpecialty> specialtySet = new HashSet<>();
+        specialtyRepo.findAll().forEach(specialtySet :: add);
+        return specialtySet;
+    }
+
+    @Override
+    public ContractorSpecialty findById(Long id) {
+        return specialtyRepo.findById(id).orElse(null);
+    }
+
+    @Override
+    public ContractorSpecialty save(ContractorSpecialty object) {
+        return specialtyRepo.save(object);
+    }
+
+    @Override
+    public void delete(ContractorSpecialty object) {
+    specialtyRepo.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+    specialtyRepo.deleteById(id);
+    }
+}
