db46c6e88916dae972ab6c4dea4304e5a2a38abf
Michael Sleeth <sleeth.ma@gmail.com>
2019-02-21 19:44:20 -0600

    Create RESTful endpoint for Contractors. Closes #66

--- hoaup-keep-web/src/main/java/com/hoaupkeep/innosol/controllers/ContractorController.java
+++ hoaup-keep-web/src/main/java/com/hoaupkeep/innosol/controllers/ContractorController.java
@@ -1,9 +1,13 @@
 package com.hoaupkeep.innosol.controllers;
 
+import com.hoaupkeep.innosol.models.Contractor;
 import com.hoaupkeep.innosol.services.ContractorService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class ContractorController {
@@ -18,5 +22,10 @@ public class ContractorController {
     public String getListView(Model model){
         model.addAttribute("contractors", contractorService.findAll());
         return "contractors/index";
+    }
+
+    @GetMapping("contractors/api")
+    public @ResponseBody Set<Contractor> getContractorAPI(){
+        return contractorService.findAll();
     }
 }
